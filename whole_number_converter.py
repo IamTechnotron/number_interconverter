7554c9543da6aef6dc3737d486daf38112dbe124
Avik Mukherjee <technotron.avik@gmail.com>
2017-08-14 08:36:26 +0000

    hexadecimal conversions are working.
    • Removed all mindless statements inside main and quantised into functions.
    • Hexadecimal conversions are now working properly. 
    • Thus now, the whole program is functional.

--- whole_number_converter.py
+++ whole_number_converter.py
@@ -1,223 +1,157 @@
-import math
+from math import pow
 
-print("-- WELCOME TO WHOLE-NUMBER SYSTEM CONVERTER --")
-print("   ==================================")
-print("[Here you can inter-convert any whole-number of base 2,8,10,16]\n")
-while True:
-    print("+------------------------------+")
-    print("| Please choose from the menu  |")
-    print("+------------------------------+\n")
-    print(" 1. Decimal      ->  Binary")
-    print(" 2. Binary       ->  Decimal")
-    print(" 3. Decimal      ->  Octal")
-    print(" 4. Octal        ->  Decimal")
-    print(" 5. Decimal      ->  Hexadecimal")
-    print(" 6. Hexadecimal  ->  Decimal")
-    print(" 7. Octal        ->  Binary")
-    print(" 8. Binary       ->  Octal")
-    print(" 9. Octal        ->  Hexadecimal")
-    print("10. Hexadecimal  ->  Octal")
-    print("11. Binary       ->  Hexadecimal")
-    print("12. Hexadecimal  ->  Binary")
-    print("13. To Exit")
-    choice = int(input("\n>>> "))
 
+def decimal_from_binary_octal(number_in, base):
+    number_in = int(number_in)
     converted_no = 0
     pos = 0
-    pos_coef = 1
+    while number_in:
+        remainder = number_in % 10
+        number_in = int(number_in / 10)
+        converted_no = converted_no + remainder * int(pow(base, pos))
+        pos = pos + 1
+    return converted_no
 
-# Decimal   ->  Binary
-    if choice == 1:
-        number_in = int(input("\nEnter the Decimal number: "))
-        while number_in:
-            remainder = number_in % 2
-            number_in = int(number_in / 2)
-            pos_coef = int(math.pow(10, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
 
-# Binary    ->  Decimal
-    elif choice == 2:
-        number_in = int(input("\nEnter the Binary number: "))
-        while number_in:
-            remainder = number_in % 10
-            number_in = int(number_in / 10)
-            pos_coef = int(math.pow(2, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
+def decimal_to_binary_octal(number_in, base):
+    number_in = int(number_in)
+    converted_no = 0
+    pos = 0
+    while number_in:
+        remainder = number_in % base
+        number_in = int(number_in / base)
+        converted_no = converted_no + remainder * int(pow(10, pos))
+        pos = pos + 1
+    return converted_no
 
-# Decimal  ->  Octal
-    elif choice == 3:
-        number_in = int(input("\nEnter the Decimal number: "))
-        while number_in:
-            remainder = number_in % 8
-            number_in = int(number_in / 8)
-            pos_coef = int(math.pow(10, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
 
-# Octal  ->  Decimal
-    elif choice == 4:
-        number_in = int(input("\nEnter the Octal number: "))
-        while number_in:
-            remainder = number_in % 10
-            number_in = int(number_in / 10)
-            pos_coef = int(math.pow(8, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
+def binary_to_from_octal(number_in, base_in, base_out):
+    number_in = int(number_in)
+    temp = decimal_from_binary_octal(number_in, base_in)
+    converted_number = decimal_to_binary_octal(temp, base_out)
+    return converted_number
 
-# Decimal   ->  Hexadecimal
-    elif choice == 5:
-        number_in = int(input("\nEnter the Decimal number: "))
-        converted_no = ''
-        while number_in:
-            remainder = number_in % 16
-            number_in = int(number_in / 16)
-            if remainder < 9:
-                converted_no = converted_no + str(remainder)
-            elif remainder == 10:
-                converted_no = converted_no + 'A'
-            elif remainder == 11:
-                converted_no = converted_no + 'B'
-            elif remainder == 12:
-                converted_no = converted_no + 'C'
-            elif remainder == 13:
-                converted_no = converted_no + 'D'
-            elif remainder == 14:
-                converted_no = converted_no + 'E'
-            elif remainder == 15:
-                converted_no = converted_no + 'F'
-            converted_no = converted_no[::-1]
 
-    elif choice == 6:
-        number_in = input("\nEnter the Hexadecimal number: ")
-        print("Sorry, Out OF Order")
+def decimal_to_hexa(number_in):
+    number_in = int(number_in)
+    set_value = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+    converted_no = ''
+    while number_in:
+        remainder = int(number_in % 16)
+        number_in = int(number_in / 16)
+        if remainder < 9:
+            converted_no = converted_no + str(remainder)
+        else:
+            converted_no = converted_no + set_value[remainder]
+    converted_no = converted_no[::-1]
+    return converted_no
 
-# Octal  ->  Binary
-    elif choice == 7:
-        number_in = int(input("\nEnter the Octal number: "))
-        while number_in:
-            remainder = number_in % 10
-            number_in = int(number_in / 10)
-            pos_coef = int(math.pow(8, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
 
-        number_in = converted_no
-        converted_no = 0
-        pos = 0
-        pos_coef = 1
-        while number_in:
-            remainder = number_in % 2
-            number_in = int(number_in / 2)
-            pos_coef = int(math.pow(10, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
+def decimal_from_hexa(number_in):
+    set_value = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    pos = 0
+    converted_no = 0
+    for element in number_in[::-1]:
+        if element in set_value:
+            temp = int(set_value[element] * int(pow(16, pos)))
+        else:
+            temp = int(element) * int(pow(16, pos))
+        pos = pos + 1
+        converted_no = converted_no + temp
+    return converted_no
 
-# Binary  ->  Octal
-    elif choice == 8:
-        number_in = int(input("\nEnter the Binary number: "))
-        while number_in:
-            remainder = number_in % 10
-            number_in = int(number_in / 10)
-            pos_coef = int(math.pow(2, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
 
-        number_in = converted_no
-        converted_no = 0
-        pos = 0
-        pos_coef = 1
-        while number_in:
-            remainder = number_in % 8
-            number_in = int(number_in / 8)
-            pos_coef = int(math.pow(10, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
+def binary_octal_to_hex(number_in, base):
+    temp = decimal_from_binary_octal(number_in, base)
+    converted_no = decimal_to_hexa(temp)
+    return converted_no
 
-# Octal  ->  Hexadecimal
-    elif choice == 9:
-        number_in = int(input("\nEnter the Octal number: "))
-        while number_in:
-            remainder = number_in % 10
-            number_in = int(number_in / 10)
-            pos_coef = int(math.pow(8, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
 
-        number_in = converted_no
-        converted_no = ''
-        number_in = int(input("\nEnter the Decimal number: "))
-        while number_in:
-            remainder = number_in % 16
-            number_in = int(number_in / 16)
-            if remainder < 9:
-                converted_no = converted_no + str(remainder)
-            elif remainder == 10:
-                converted_no = converted_no + 'A'
-            elif remainder == 11:
-                converted_no = converted_no + 'B'
-            elif remainder == 12:
-                converted_no = converted_no + 'C'
-            elif remainder == 13:
-                converted_no = converted_no + 'D'
-            elif remainder == 14:
-                converted_no = converted_no + 'E'
-            elif remainder == 15:
-                converted_no = converted_no + 'F'
-            converted_no = converted_no[::-1]
+def binary_octal_from_hex(number_in, base):
+    temp = decimal_from_hexa(number_in)
+    converted_no = decimal_to_binary_octal(temp, base)
+    return converted_no
 
-    elif choice == 10:
-        number_in = input("\nEnter the Hexadecimal number: ")
-        print("Sorry, Out OF Order")
 
-# Binary  ->  Hexadecimal
-    elif choice == 11:
-        number_in = int(input("\nEnter the Binary number: "))
-        while number_in:
-            remainder = number_in % 10
-            number_in = int(number_in / 10)
-            pos_coef = int(math.pow(2, pos))
-            converted_no = converted_no + remainder * pos_coef
-            pos = pos + 1
+if __name__ == '__main__':
+    print("-- WELCOME TO WHOLE-NUMBER SYSTEM CONVERTER --")
+    print("   ========================================")
+    print("[Here you can inter-convert any whole-number of base 2,8,10,16]\n")
+    while True:
+        print("+------------------------------+")
+        print("| Please choose from the menu  |")
+        print("+------------------------------+\n")
+        print(" 1. Decimal      ->  Binary")
+        print(" 2. Binary       ->  Decimal")
+        print(" 3. Decimal      ->  Octal")
+        print(" 4. Octal        ->  Decimal")
+        print(" 5. Decimal      ->  Hexadecimal")
+        print(" 6. Hexadecimal  ->  Decimal")
+        print(" 7. Octal        ->  Binary")
+        print(" 8. Binary       ->  Octal")
+        print(" 9. Octal        ->  Hexadecimal")
+        print("10. Hexadecimal  ->  Octal")
+        print("11. Binary       ->  Hexadecimal")
+        print("12. Hexadecimal  ->  Binary")
+        print("13. To Exit")
+        choice = int(input("\n>>> "))
 
-            number_in = converted_no
-            converted_no = ''
-            number_in = int(input("\nEnter the Decimal number: "))
-            while number_in:
-                remainder = number_in % 16
-                number_in = int(number_in / 16)
-                if remainder < 9:
-                    converted_no = converted_no + str(remainder)
-                elif remainder == 10:
-                    converted_no = converted_no + 'A'
-                elif remainder == 11:
-                    converted_no = converted_no + 'B'
-                elif remainder == 12:
-                    converted_no = converted_no + 'C'
-                elif remainder == 13:
-                    converted_no = converted_no + 'D'
-                elif remainder == 14:
-                    converted_no = converted_no + 'E'
-                elif remainder == 15:
-                    converted_no = converted_no + 'F'
-                converted_no = converted_no[::-1]
+        if choice == 1:
+            number = int(input("\nEnter the Decimal number: "))
+            convert = decimal_to_binary_octal(number, 2)
 
-# Hexadecimal  ->  Binary
-    elif choice == 12:
-        number_in = input("\nEnter the Hexadecimal number: ")
-        print("Sorry, Out OF Order")
-# EXIT
-    elif choice == 13:
-        print("\nExiting...")
-        break
- 
-    else:
-        print("ERROR : WRONG INPUT\nPlease choose correctly !")
-        continue
+        elif choice == 2:
+            number = int(input("\nEnter the Binary number: "))
+            convert = decimal_from_binary_octal(number, 2)
 
-    print("converted no :", converted_no)
-    
+        elif choice == 3:
+            number = int(input("\nEnter the Decimal number: "))
+            convert = decimal_to_binary_octal(number, 8)
+
+        elif choice == 4:
+            number = int(input("\nEnter the Binary number: "))
+            convert = decimal_from_binary_octal(number, 8)
+
+        elif choice == 5:
+            number = int(input("\nEnter the Decimal number: "))
+            convert = decimal_to_hexa(number)
+
+        elif choice == 6:
+            number = input("\nEnter the Hexadecimal number: ")
+            convert = decimal_from_hexa(number)
+
+        elif choice == 7:
+            number = int(input("\nEnter the Octal number: "))
+            convert = binary_to_from_octal(number, 8, 2)
+
+        elif choice == 8:
+            number = int(input("\nEnter the Binary number: "))
+            convert = binary_to_from_octal(number, 2, 8)
+
+        elif choice == 9:
+            number = int(input("\nEnter the Octal number: "))
+            convert = binary_octal_to_hex(number, 8)
+
+        elif choice == 10:
+            number = input("\nEnter the Hexadecimal number: ")
+            convert = binary_octal_from_hex(number, 8)
+
+        elif choice == 11:
+            number = int(input("\nEnter the Binary number: "))
+            convert = binary_octal_to_hex(number, 2)
+
+        elif choice == 12:
+            number = input("\nEnter the Hexadecimal number: ")
+            convert = binary_octal_from_hex(number, 2)
+
+        elif choice == 13:
+            print("\nExiting...")
+            break
+
+        else:
+            print("ERROR : WRONG INPUT\nPlease choose correctly from the menu!")
+            continue
+
+        print("converted no :", convert)
     print("\n---------------------------------------------------------------------------\n")
-
 
